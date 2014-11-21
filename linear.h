060c1e704d87c949f97284f55e92d28f9e832ab2
Artem Kirienko <artem.kirienko@gmail.com>
2014-11-21 11:59:49 +0300

    LINEAR_SIZE set to fit Core i7 L1 cache

--- linear.h
+++ linear.h
@@ -4,7 +4,7 @@
 double* solveLinear(double** matrix, double* f, long size);
 double* parallelSolveLinear(double** matrix, double* f, long size);
 
-#define LINEAR_SIZE 20000
+#define LINEAR_SIZE 16000
 double testSolve();
 double testParallelSolve();
 
