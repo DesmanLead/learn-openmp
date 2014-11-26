ab9a00b05750bf50134e4d8a42e7bf1c0445eda4
Artem Kirienko <artem.kirienko@gmail.com>
2014-11-26 21:32:27 +0300

    Matrix initialization fixed

--- utils.cpp
+++ utils.cpp
@@ -48,6 +48,9 @@ double** generateDiagonallyDominantMatrix(long size) {
     double** matrix = new double*[size];
     for (int i = 0; i < size; ++i) {
         matrix[i] = new double[size];
+        for (int j = 0; j < size; ++j) {
+            matrix[i][j] = 0;
+        }
     }
 
     if (size == 1) {
